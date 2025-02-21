e6210680ab8fda40c347fc2b797d8d2640ee1185
devUn9 <hyeonungmoon@gmail.com>
2025-02-21 18:00:05 +0900

    study6

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //로딩바 시작화면
+            //게임 스토리1
+            
+            Console.WriteLine("엔터를 누르면 스토리가 진행됩니다.");
+            Console.Write("\n▶ 시작한다");
+
+
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("□□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.WriteLine("■□□□□□□□");
+            Thread.Sleep(100);
+            Console.Clear();
+
+            Console.WriteLine("■■□□□□□□");
+            Thread.Sleep(100);
+            Console.Clear();
+
+            Console.WriteLine("■■■□□□□□");
+            Thread.Sleep(100);
+            Console.Clear();
+
+            Console.WriteLine("■■■■□□□□");
+            Thread.Sleep(100);
+            Console.Clear();
+
+            Console.WriteLine("■■■■■□□□");
+            Thread.Sleep(100);
+            Console.Clear();
+
+            Console.WriteLine("■■■■■■□□");
+            Thread.Sleep(100);
+            Console.Clear();
+
+            Console.WriteLine("■■■■■■■□");
+            Thread.Sleep(100);
+            Console.Clear();
+
+            Console.WriteLine("■■■■■■■■");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.WriteLine("당신은 지루하다..");
+            Console.WriteLine("\n▶ 산책을 나선다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("앞에서 무슨 소리가 들린다.");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.WriteLine("냐...");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.WriteLine("냐옹.....");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.WriteLine("냐아아옹~~~~");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+
+            Console.WriteLine(" /\\_/\\  \r\n( -_- )  \r\n /     \\  \r\n(________)  ");
+            Console.WriteLine("야생의 고양이를 발견했다!");
+
+            Console.WriteLine("\n▶ 츄르를 준다.");
+            Console.ReadLine();
+
+
+
+            string[] catFrames = new string[]
+        {
+            @" /\_/\
+( o.o )
+ > ^  <
+(_______)
+고양이가 기뻐한다!",
+            @" /\_/\ 
+( o.o ) 
+ < ^  >  
+(_______)
+고양이가 기뻐한다!",
+            @" /\_/\  
+( o.o )  
+ > ~  <  
+(_______)
+고양이가 기뻐한다!"
+
+        };
+
+            while (true)
+            {
+                foreach (string frame in catFrames)
+                {
+                    Console.Clear();
+                    Console.WriteLine(frame);
+                    Thread.Sleep(500); // 0.5초마다 변경
+                }
+            }
+
+            Console.WriteLine("냐옹!");
+
+        }
+    }
+}
--- studyReadLine/studyReadLine/Program.cs
+++ studyReadLine/studyReadLine/Program.cs
@@ -17,12 +17,62 @@ namespace studyReadLine
             Console.WriteLine($"안녕하세요, {userName}님!");*/
 
             //문자열을 정수로 변환
-            Console.Write("나이를 입력하세요: ");
-            string input = Console.ReadLine(); // 사용자로부터 입력받기
-            int age = int.Parse(input); //문자열을 정수로 변환
+            //Console.Write("나이를 입력하세요: ");
+            //string input = Console.ReadLine(); // 사용자로부터 입력받기
+            //int age = int.Parse(input); //문자열을 정수로 변환
 
-            Console.WriteLine($"내년에는 {age + 1}살이 되겠군요!");
-            
+            //Console.WriteLine($"내년에는 {age + 1}살이 되겠군요!");
+            //Console.WriteLine("내년에는" + age + "살이 되겠군요!")
+            //;
+            //Console.WriteLine("내년에는 {0}살이 되겠군요!", age);
+
+            //루인 스킬 피해 : 4.5% 입력받기
+
+            Console.Write("루인 스킬 피해를 입력하세요: ");
+            string input1 = Console.ReadLine();
+            float Skill_Damage = float.Parse(input1);
+
+            Console.Write("카드 게이지 획득량: ");
+            float Card_Gage = float.Parse(Console.ReadLine());
+
+            Console.Write("각성기 피해: ");
+            float Ult_Damage = float.Parse(Console.ReadLine());
+
+            Console.Write("최대 마나: ");
+            float Max_Mana = float.Parse(Console.ReadLine());
+
+            Console.Write("전투 중 마나 회복량: ");
+            float CombatManaRegen = float.Parse(Console.ReadLine());
+
+            Console.Write("비전투 중 마나 회복량: ");
+            float ManaRegenOutofCombat = float.Parse(Console.ReadLine());
+
+            Console.Write("이동 속도: ");
+            float MoveSpeed = float.Parse(Console.ReadLine());
+
+            Console.Write("탈것 속도: ");
+            float rideSpeed = float.Parse(Console.ReadLine());
+
+            Console.Write("운반 속도: ");
+            float CarrySpeed = float.Parse(Console.ReadLine());
+
+            Console.Write("스킬 재사용 대기시간 감소: ");
+            float CooldownReduction = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine($"활동{"▼",41}");
+            Console.WriteLine($"루인 스킬 피해 {Skill_Damage,30}%");
+            Console.WriteLine($"카드 게이지 획득량 {Card_Gage,26}%");
+            Console.WriteLine($"각성기 피해 {Ult_Damage,33}%");
+            Console.WriteLine($"최대 마나 {Max_Mana,36}");
+            Console.WriteLine($"전투 중 마나 회복량 {CombatManaRegen,26}");
+            Console.WriteLine($"비전투 중 마나 회복량 {ManaRegenOutofCombat,24}");
+            Console.WriteLine($"이동 속도 {MoveSpeed,35}%");
+            Console.WriteLine($"탈 것 속도 {rideSpeed,34}%");
+            Console.WriteLine($"운반 속도 {CarrySpeed,35}%");
+            Console.WriteLine($"스킬 재사용 대기시간 감소 {CooldownReduction,19}%");
+      
         }
     }
 }
